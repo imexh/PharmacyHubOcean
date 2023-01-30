286397333a726995572fa71604ca7d9d09fcec8e
imexh <chandikaranawella@gmail.com>
2023-01-31 01:08:36 +0530

    first commit

--- src/main/java/com/noobz/pharmacyhub/Basic/LandingPage/LandingController.java
+++ src/main/java/com/noobz/pharmacyhub/Basic/LandingPage/LandingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LandingController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String viewLandingPage(Model model)
     {
         return "index";
